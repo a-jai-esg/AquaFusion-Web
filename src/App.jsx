e5823853614bfc2e968831721b38f995c4f64fdd
Elijah Nicholas <ashesg1117@gmail.com>
2023-06-27 12:06:12 +0800

    Added components and stylings for Homepage, initial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Homepage from './Pages/Homepage'
+//import './App.css'
 
 function App() {
-  
+
+  // we'll integrate axios here to get data from firebase
   useEffect(() => {
     document.title = "AquaFusion: Aquaponics IoT Monitoring System";
   });
 
   return (
     <>
-      <p>Hello, World.</p>    
+      <Homepage isLoggedIn={true} username={"Jennie Kim"} profilePicture={"https://cdns-images.dzcdn.net/images/artist/d246561939a0b8000f88d7d59df32f54/500x500.jpg"}/>
     </>
   )
 }
